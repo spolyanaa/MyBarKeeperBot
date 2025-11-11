2111febf98e83ce34ce64491b55947ca1367f502
AlexKCoherent <alexkaraychetsev@coherentsolutions.com>
2025-11-11 16:31:50 +0300

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /root/projects/myBarKeeperBot
 git fetch origin
-git reset --hard origin/release
+git reset --hard origin/main
 source venv/bin/activate
 pip install -r requirements.txt
 deactivate
